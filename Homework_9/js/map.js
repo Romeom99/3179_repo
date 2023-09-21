918d9cd33546f2cfce4d2ee88a699d565542d10c
Romeom99 <62103670+Romeom99@users.noreply.github.com>
2023-09-21 13:39:52 +1000

    new json file

--- Homework_9/js/map.js
+++ Homework_9/js/map.js
@@ -1,4 +1,4 @@
-var loc = "chlor_map.vg.json"
+var loc = "js/chlor_map.vg.json"
 vegaEmbed('#for_lev_map', loc).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
