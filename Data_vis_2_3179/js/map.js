c9b1b9ec9c90faebdd75bcc338ac9ba3255abecb
Romeom99 <62103670+Romeom99@users.noreply.github.com>
2023-09-21 12:44:38 +1000

    Files moved

--- Data_vis_2_3179/js/map.js
+++
@@ -1,4 +0,0 @@
-var loc = "chlor_map.vg.json"
-vegaEmbed('#for_lev_map', loc).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-  }).catch(console.error);
