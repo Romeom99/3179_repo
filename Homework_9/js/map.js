96d4dbca8afd5cfc3099490a24464424cb49f1c9
Romeom99 <62103670+Romeom99@users.noreply.github.com>
2023-09-23 13:27:51 +1000

    map feature working, no tooltip

--- Homework_9/js/map.js
+++ Homework_9/js/map.js
@@ -1,4 +1,4 @@
 var loc = "js/chlor_map.vg.json"
-vegaEmbed('#for_lev_map', loc).then(function(result) {
+vegaEmbed('#for_lev_map', loc, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
