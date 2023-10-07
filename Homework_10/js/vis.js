96ec83dccc8a05990d9bf1d1592a9d1a1253f8a4
Romeom99 <62103670+Romeom99@users.noreply.github.com>
2023-10-07 22:59:58 +1100

    data added for week 10

--- Homework_10/js/vis.js
+++ Homework_10/js/vis.js
@@ -1,4 +1,4 @@
-var loc = "js/chlor_map.vg.json"
-vegaEmbed('#for_lev_map', loc, { "actions": false }).then(function(result) {
+var loc = "js/vis10.vg.json"
+vegaEmbed('#vis', loc, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
