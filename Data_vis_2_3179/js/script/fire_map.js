98bd0721b2d3a45c350129a617e8ddafe0f8a7da
Romeom99 <62103670+Romeom99@users.noreply.github.com>
2023-10-09 16:35:26 +1100

    Set up files for data vis2

--- Data_vis_2_3179/js/script/fire_map.js
+++ Data_vis_2_3179/js/script/fire_map.js
@@ -1,4 +1,4 @@
-var loc = "js/chlor_map.vg.json"
+var loc = "js/vega_json/fire_map.vg.json"
 vegaEmbed('#map', loc, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
