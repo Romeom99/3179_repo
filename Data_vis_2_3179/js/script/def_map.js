b18c1d32d0f961985442872655aad4d4c493a852
Romeom99 <62103670+Romeom99@users.noreply.github.com>
2023-10-12 14:52:37 +1100

    html for critque

--- Data_vis_2_3179/js/script/def_map.js
+++ Data_vis_2_3179/js/script/def_map.js
@@ -1,4 +1,4 @@
-var loc = "js/vega_json/line_def.vg.json"
-vegaEmbed('#line_defor', loc, { "actions": false }).then(function(result) {
+var loc = "js/vega_json/def_map.vg.json"
+vegaEmbed('#def_map', loc, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
