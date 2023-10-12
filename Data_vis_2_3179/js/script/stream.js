aa3aafb065b066f60e43f86e78b0e04cd71cb235
Romeom99 <62103670+Romeom99@users.noreply.github.com>
2023-10-12 16:11:34 +1100

    sream graph data and frame added

---
+++ Data_vis_2_3179/js/script/stream.js
@@ -0,0 +1,4 @@
+var loc = "js/vega_json/stream.vg.json"
+vegaEmbed('#test', loc, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
