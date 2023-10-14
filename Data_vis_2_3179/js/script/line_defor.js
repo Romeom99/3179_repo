b69ed07ad890ad5158a36df8f7ba45a9350eb032
Romeom99 <62103670+Romeom99@users.noreply.github.com>
2023-10-14 19:01:15 +1100

    worked on html

--- Data_vis_2_3179/js/script/line_defor.js
+++ Data_vis_2_3179/js/script/line_defor.js
@@ -1,4 +1,4 @@
 var loc = "js/vega_json/line_def.vg.json"
-vegaEmbed('#line_def', loc, { "actions": false }).then(function(result) {
+vegaEmbed('#line_defor', loc, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
